dd8fa73f28a853ea1afc64bc2248ffede044716b
jettg15 <jetthuffhines@gmail.com>
2023-08-27 21:34:34 -0500

    js file for questions complete

---
+++ Assets/JavaScript/questions.js
@@ -0,0 +1,33 @@
+var questionArr = [
+    {
+        question: "Commonly used data types DO Not include:",
+        choices:["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+
+    },
+
+    {
+        question: "Arrays in javascript can be used to store ______.",
+        choices:["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        question: "The condition in an if / else statement is enclosed with ______.", 
+        choices:["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis"
+    },
+
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices:["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing conent to the debugger is:",
+        choices:["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+
+];
--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,7 +1,21 @@
 // vars
 var secondsLeft = 75;
-// start button
-function startButton()
+var time = questionArr.length * 15;
+
+
+
+
+
+
+
+
+// start button/Start quiz
+function startButton(){
+    var startQuiz = document.getElementById("intro");
+    startQuiz.setAttribute("class", "intro hide")
+
+
+}
 // timer
 function setTimer() {
     // Sets interval in variable
@@ -36,17 +50,17 @@ function endScreen() {
 
 // questions seperate js file?
 
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
 
 
 
